654ec5c848bf852b5c36a3e1dc1c87963a068030
hasan <hasan@coderhasan.com>
2024-03-02 13:12:33 +0500

    first commit

---
+++ assignment 3/main.js
@@ -0,0 +1,8 @@
+"use strict";
+// Assignment # 3
+// Storing the famouse qoute and it's author
+let author = "Hussain";
+let qoute = "Nothing is impossible because impossible iself I'm possible";
+//printing the message
+console.log(`Once  ${author} said
+    "${qoute}"`);
---
+++ assignment 3/main.ts
@@ -0,0 +1,12 @@
+// Assignment # 3
+
+// Storing the famouse qoute and it's author
+
+let author:string = "Hussain"
+
+let qoute:string = "Nothing is impossible because impossible iself I'm possible"
+
+//printing the message
+
+console.log(`Once  ${author} said
+    "${qoute}"`)
